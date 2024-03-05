07d98d43a7f5c0bc7e959b0d837500793ce53dad
rbenites <alrrebe@gmail.com>
2024-03-05 17:35:47 -0500

    HomeWork M2 Clase 02

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,23 +1,36 @@
 //console.log(tempData);
 
+function createMovie(movie) {
+    const card = document.createElement('div');
+        card.classList.add("movie-card");
+    
+        card.innerHTML = `
+        <img src="${movie.poster}" alt="${movie.title}" class="poster">
+        <div class="movie-info">
+            <h3>${movie.title} (${movie.year})</h3>
+            <p><strong>Director:</strong> ${movie.director}</p>
+            <p><strong>Duration:</strong> ${movie.duration}</p>
+            <p><strong>Genero:</strong> ${movie.genre.join(', ')}</p>
+            <p><strong>Rate:</strong> ${movie.rate}</p>
+        </div>
+        `;
+
+        return card;
+}
+
+function addMovieToContainer(container, movies) {
+    movies.forEach(movie => {
+        const card = createMovie(movie)
+        container.appendChild(card);
+    });
+}
+
 
 const container = document.getElementById("container");
 
-tempData.forEach(movie => {
+$.get("https://students-api.2.us-1.fl0.io/movies", data => {
+    //console.log(data);
+    addMovieToContainer(container, data)
+});
 
-    const card = document.createElement('div');
-    card.classList.add("movie-card");
 
-    card.innerHTML = `
-    <img src="${movie.poster}" alt="${movie.title}" class="poster">
-    <div class="movie-info">
-        <h3>${movie.title} (${movie.year})</h3>
-        <p><strong>Director:</strong> ${movie.director}</p>
-        <p><strong>Duration:</strong> ${movie.duration}</p>
-        <p><strong>Genero:</strong> ${movie.genre}</p>
-        <p><strong>Rate:</strong> ${movie.rate}</p>
-    </div>
-    `;
-
-    container.appendChild(card);
-})
