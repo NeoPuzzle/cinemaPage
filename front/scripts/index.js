d6d58f264d3b8b19d5cf4865877ca22cdc7802b2
rbenites <alrrebe@gmail.com>
2024-03-11 23:33:32 -0500

    HomeWork M2 Clase 06

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,9 +1,18 @@
-const {addMovieToContainer} = require("./models/cardsMovie")
+const axios = require("axios");
+const {addMovieToContainer} = require("./models/cardsMovie");
 
 const container = document.getElementById("container-movies");
 
-$.get("https://students-api.2.us-1.fl0.io/movies", data => {
-    addMovieToContainer(container, data)
-});
+const fetchMovies = async () => {
+    try {
+        const data = await axios.get("https://students-api.up.railway.app/movies");
+        addMovieToContainer(container, data.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+}
+
+fetchMovies();
 
 
